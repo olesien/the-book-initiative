a7f44b90e89b612a88cd2d1d5b7676b3de59b6c2
olesien <olesien2@gmail.com>
2023-12-14 18:31:10 +0100

    added icon to app

--- src/main/java/com/books/thebookinitiative/BookApplication.java
+++ src/main/java/com/books/thebookinitiative/BookApplication.java
@@ -4,9 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 public class BookApplication extends Application {
     @Override
@@ -14,11 +18,17 @@ public class BookApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(BookApplication.class.getResource("books-view.fxml"));
         Parent parent = fxmlLoader.load();//Load the fxml
 
+        Image icon = new Image("file:images/bookicon.png");
+        System.out.println(icon.getUrl());
+        System.out.println(icon.getWidth());
+        stage.getIcons().add(icon);
+
         BooksController controller = fxmlLoader.getController(); //Get controller ref before scene is made
 
         Scene scene = new Scene(parent, 600, 600);
         stage.setScene(scene);
         controller.init();
+
         stage.setTitle("The Book Initiative");
         stage.show();
     }
